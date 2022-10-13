1752a3809dbf4d05ab080bdc12c6ce53abb4158b
Connor Feeley <cfeeley@cfeeley.org>
2022-10-13 00:00:00 -0400

    feat: set compile-command in dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; Set project's compilation command to `publish`
+((nil . ((compile-command . "publish"))))
