73ed2eb6af0b125e4ffea5175d1f4438a5b872e3
Connor Feeley <git@cfeeley.org>
2023-11-27 18:48:22 -0500

    nix: in-repo flake and shell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Flake for personal website.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+
+  outputs = { self, nixpkgs, flake-utils, devshell, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShells.default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+
+            overlays = [ devshell.overlays.default ];
+          };
+        in
+        pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        };
+    });
+}
