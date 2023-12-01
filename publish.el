d411f27a9843dd793af2b011e2cae3bf9697e6ac
Connor Feeley <git@cfeeley.org>
2023-12-01 09:00:00 -0500

    stats: fix URL

--- publish.el
+++ publish.el
@@ -176,7 +176,7 @@
             (link (@ (rel "stylesheet") (href ,(concat dw/site-url "/css/code.css"))))
             (link (@ (rel "stylesheet") (href ,(concat dw/site-url "/css/site.css"))))
             (script (@ (defer "defer")
-                       (data-goatcounter "cfeeley.org")
+                       (data-goatcounter "stats.cfeeley.org")
                        (src "https://stats.cfeeley.org/count.js"))
                     ;; Empty string to cause a closing </script> tag
                     "")
