77ec99d0e973a210a1ad533fe7828c06292d51f3
Connor Feeley <git@cfeeley.org>
2023-12-14 12:18:26 -0500

    index: change tech icon

--- publish.el
+++ publish.el
@@ -358,7 +358,7 @@
           (("faArchive" "\\faArchive" nil "<i aria-hidden='true' class='fa fa-archive'></i>" "" "" "")
            ("faRss" "\\faRss" nil "<i aria-hidden='true' class='fa fa-rss'></i>" "" "" "")
            ("faBookmark" "\\faBookmark" nil "<i aria-hidden='true' class='fa fa-bookmark'></i>" "" "" "")
-           ("faCode" "\\faCode" nil "<i aria-hidden='true' class='fa fa-code'></i>" "" "" "")
+           ("faCode" "\\faCode" nil "<i aria-hidden='true' class='fa fa-terminal'></i>" "" "" "")
            ("faBike" "\\faBike" nil "<i aria-hidden='true' class='fa fa-bicycle'></i>" "" "" "")
            ("faToronto" "\\faBike" nil "<i aria-hidden='true' class='fa fa-train-subway'></i>" "" "" "")
            ("faGithub" "\\faGithub" nil "<i aria-hidden='true' class='fa fa-github'></i>" "" "" "")
