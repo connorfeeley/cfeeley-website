aa809e39356e7a27a36564300c6d93a03984480a
Connor Feeley <git@cfeeley.org>
2023-12-13 22:19:05 -0500

    nix: init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Flake for personal website.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    devshell.url = "github:numtide/devshell";
+
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+
+    # flake-parts and friends.
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
+    treefmt-nix = { url = "github:numtide/treefmt-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, moduleWithSystem, flake-parts-lib, ... }:
+      {
+        debug = true;
+        systems = nixpkgs.lib.systems.flakeExposed;
+        imports = [
+          inputs.treefmt-nix.flakeModule
+          inputs.flake-root.flakeModule
+          inputs.devshell.flakeModule
+        ];
+
+        flake = { };
+
+        perSystem = { self', config, pkgs, ... }:
+          let
+            emacsForPublish = ((pkgs.emacsPackagesFor pkgs.emacs29).emacsWithPackages (
+              epkgs: with epkgs; [ esxml htmlize webfeeder ]
+            ));
+          in
+          {
+            packages.default = pkgs.callPackage ./nix/cfeeley-website/default.nix { inherit emacsForPublish; publishUrl = null; };
+
+            devshells.default = {  };
+
+            # Treefmt configuration.
+            treefmt.config = {
+              inherit (config.flake-root) projectRootFile;
+              package = pkgs.treefmt;
+              programs.nixpkgs-fmt.enable = true;
+            };
+          };
+      });
+}
---
+++ nix/cfeeley-website/default.nix
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2023 Connor Feeley
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+{ lib, stdenv, git, emacsForPublish, publishUrl }:
+
+stdenv.mkDerivation {
+  name = "cfeeley-website";
+
+  src = ../../.;
+
+  buildInputs = [ emacsForPublish git ];
+
+  patchPhase = lib.optionalString (publishUrl != null) ''
+    substituteInPlace publish.el --replace "http://localhost:8080" "${publishUrl}"
+  '';
+
+  buildPhase = ''
+    mkdir -p $out
+    emacs -Q --batch -l $src/publish.el --funcall duncan-publish-all
+    cp -r $src/public/* $out
+  '';
+}
