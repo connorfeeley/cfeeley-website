56453c620ad5b5a584e2a1c3d557319182eac2ac
Connor Feeley <git@cfeeley.org>
2023-12-13 23:16:37 -0500

    add bikes

--- publish.el
+++ publish.el
@@ -348,6 +348,7 @@
            ("faRss" "\\faRss" nil "<i aria-hidden='true' class='fa fa-rss'></i>" "" "" "")
            ("faBookmark" "\\faBookmark" nil "<i aria-hidden='true' class='fa fa-bookmark'></i>" "" "" "")
            ("faCode" "\\faCode" nil "<i aria-hidden='true' class='fa fa-code'></i>" "" "" "")
+           ("faBike" "\\faBike" nil "<i aria-hidden='true' class='fa fa-bicycle'></i>" "" "" "")
            ("faGithub" "\\faGithub" nil "<i aria-hidden='true' class='fa fa-github'></i>" "" "" "")
            ("faGraduationCap" "\\faGraduationCap" nil "<i aria-hidden='true' class='fa fa-graduation-cap'></i>" "" "" "")
            ("faImage" "\\faImage" nil "<i aria-hidden='true' class='fa fa-image'></i>" "" "" ""))))
